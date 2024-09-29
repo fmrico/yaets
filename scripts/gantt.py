f622a268cc8bb8ea1dc28fd90b63ad8fcfa269e6
Francisco Martín Rico <fmrico@gmail.com>
2024-09-29 12:08:07 +0200

    fix CI
    Signed-off-by: Francisco Martín Rico <fmrico@gmail.com>

--- scripts/gantt.py
+++ scripts/gantt.py
@@ -1,12 +1,29 @@
 #!/usr/bin/python
 
-import matplotlib.pyplot as plt
+# Copyright 2024 Intelligent Robotics Lab
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
 import argparse
-import re
+
 import matplotlib.cm as cm
+import matplotlib.pyplot as plt
+
 
 def ns_to_ms(ns):
     return ns / 1_000_000
+
 
 def read_traces(trace_file, max_traces=None):
     traces = []
@@ -24,15 +41,17 @@ def read_traces(trace_file, max_traces=None):
                 traces.append((function, start_ms, duration_ms))
     return traces
 
+
 def create_gantt_chart(traces):
-    functions = list(set(function for function, _, _ in traces))
+    functions = list({function for function, _, _ in traces})
     colors = cm.get_cmap('tab10', len(functions))
     colors_by_function = {function: colors(i) for i, function in enumerate(functions)}
 
     fig, ax = plt.subplots(figsize=(10, 6))
 
     for i, (function, start_ms, duration_ms) in enumerate(traces):
-        ax.barh(function, duration_ms, left=start_ms, height=0.4, color=colors_by_function[function])
+        ax.barh(function, duration_ms, left=start_ms, height=0.4,
+                color=colors_by_function[function])
 
     ax.set_xlabel('Time (ms)')
     ax.set_ylabel('Functions')
@@ -41,16 +60,19 @@ def create_gantt_chart(traces):
 
     plt.show()
 
+
 def main():
     parser = argparse.ArgumentParser(description='Generate a Gantt chart from a trace file.')
     parser.add_argument('trace_file', type=str, help='Trace file to process')
-    parser.add_argument('--max_traces', type=int, default=None, help='Maximum number of traces to display')
+    parser.add_argument('--max_traces', type=int, default=None,
+                        help='Maximum number of traces to display')
 
     args = parser.parse_args()
 
     traces = read_traces(args.trace_file, args.max_traces)
 
     create_gantt_chart(traces)
+
 
 if __name__ == '__main__':
     main()
--- scripts/histogram.py
+++ scripts/histogram.py
@@ -1,11 +1,28 @@
 #!/usr/bin/python
 
-import matplotlib.pyplot as plt
+# Copyright 2024 Intelligent Robotics Lab
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
 import argparse
-import re
+
+import matplotlib.pyplot as plt
+
 
 def ns_to_ms(ns):
     return ns / 1_000_000
+
 
 def read_traces(trace_file, max_traces=None):
     traces = []
@@ -23,13 +40,18 @@ def read_traces(trace_file, max_traces=None):
                 traces.append((function, start_ms, end_ms))
     return traces
 
+
 def filter_traces_by_function(traces, target_function):
-    return [(start_ms, end_ms) for function, start_ms, end_ms in traces if function == target_function]
+    return [(start_ms, end_ms) for
+            function, start_ms, end_ms in traces if function == target_function]
+
 
 def calculate_intervals_between_starts(traces):
     ordered_traces = sorted(traces, key=lambda x: x[0])
-    intervals = [ordered_traces[i+1][0] - ordered_traces[i][0] for i in range(len(ordered_traces) - 1)]
+    intervals = [ordered_traces[i+1][0] - ordered_traces[i][0] for
+                 i in range(len(ordered_traces) - 1)]
     return intervals
+
 
 def create_histogram(intervals, target_function, num_bins):
     plt.hist(intervals, bins=num_bins, edgecolor='black', log=True)
@@ -38,12 +60,17 @@ def create_histogram(intervals, target_function, num_bins):
     plt.title(f'Histogram of intervals between starts of "{target_function}"')
     plt.show()
 
+
 def main():
-    parser = argparse.ArgumentParser(description='Generate a Gantt chart or histogram from a trace file.')
+    parser = argparse.ArgumentParser(
+        description='Generate a Gantt chart or histogram from a trace file.')
     parser.add_argument('trace_file', type=str, help='Trace file to process')
-    parser.add_argument('--function', type=str, required=True, help='Name of the function for the histogram')
-    parser.add_argument('--max_traces', type=int, default=None, help='Maximum number of traces to display')
-    parser.add_argument('--bins', type=int, default=10, help='Number of bins (X-axis resolution) for the histogram')
+    parser.add_argument('--function', type=str, required=True,
+                        help='Name of the function for the histogram')
+    parser.add_argument('--max_traces', type=int, default=None,
+                        help='Maximum number of traces to display')
+    parser.add_argument('--bins', type=int, default=10,
+                        help='Number of bins (X-axis resolution) for the histogram')
 
     args = parser.parse_args()
 
@@ -52,12 +79,14 @@ def main():
     function_traces = filter_traces_by_function(traces, args.function)
 
     if len(function_traces) < 2:
-        print(f"There are not enough executions of the function '{args.function}' to calculate intervals.")
+        print(f"There are not enough executions of the function '{args.function}' "
+              'to calculate intervals.')
         return
 
     intervals = calculate_intervals_between_starts(function_traces)
 
     create_histogram(intervals, args.function, args.bins)
+
 
 if __name__ == '__main__':
     main()
